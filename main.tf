aa9eb31f3ca489d20cb8455db6883ebfc57277d4
T1kcan <97545634+T1kcan@users.noreply.github.com>
2022-09-26 09:23:58 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.9.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
 resource "aws_security_group" "sec-gr" {
   name = "test"
   description = "test1"
